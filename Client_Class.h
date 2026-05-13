30382ba92ac4653a5d61f07ebfa23892b1e213a9
atikaalhinai64 <atika.alhinai64@gmail.com>
2026-05-13 10:01:18 +0400

    Add files via upload

---
+++ Client_Class.h
@@ -0,0 +1,21 @@
+#include <string>
+using namespace std;
+
+class Client {
+private:
+    string name;
+    int number;
+    string email;
+
+public:
+    Client();
+    Client(string n, int num, string e);
+
+    void setName(string n);
+    void setNumber(int num);
+    void setEmail(string e);
+
+    void display();
+
+    ~Client();
+};
