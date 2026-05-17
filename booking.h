f153a5a54ee6d452a5da089d8eb334e887508961
reem-35 <Alsiyabireem204@gmail.com>
2026-05-17 16:20:37 +0400

    Add files via upload

---
+++ booking.h
@@ -0,0 +1,25 @@
+
+
+#ifndef BOOKING_EVENT_BOOKING_H
+#define BOOKING_EVENT_BOOKING_H
+
+
+# include <iostream>
+using namespace std;
+#include "event.h"
+#include "Entertainment.h"
+#include "decoration.h"
+#include "SERVICE OOP TEEB.h"
+
+class Booking{
+private:
+Event event;
+Entertainment entertainment;
+Decoration decoration;
+Service service;
+public:
+Booking();
+void display();
+void total_price();
+~Booking();
+};
