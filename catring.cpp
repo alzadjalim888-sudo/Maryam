ca5c38ecf8c7d55b760f8e43fd28cbc933d4468d
24-0153 <24-0153@student.gutech.edu.om>
2026-05-19 19:23:34 +0400

    Update catring.cpp

--- catring.cpp
+++ catring.cpp
@@ -1,27 +1,27 @@
-#include"catring.h"
+// Catring.cpp
+#include <iostream>
+using namespace std;
 
-catring::catring(int num){num_of_people=num;} // constructer
+#include "Catring.h"
 
+Catring::Catring(int num): Service(0, 0.0, false) {
+    num_of_people = num;
+}
 
-
-double catring::calculateprice() {
-    //using the num of people from the event class
-    switch (num_of_people){ // switch method
-        case 1:
-            return 100;
-            break;
-        case 2:
-            return 130;
-            break;
-        case 3:
-            return 150;
-            break;
+double Catring::calculatePrice() {
+    switch (num_of_people) {
+        case 1: return 100;
+        case 2: return 130;
+        case 3: return 150;
+        default: return 0;
     }
 }
 
-void catring::display(){
-    cout<<"______catring service______"<<endl;
-    cout<<"serviceID:"<<serviceID<<endl;
-    cout<<"number of people:"<<num_of_people<<endl;
-    cout<<"price:"<<calculateprice()<<endl;
+void Catring::display() {
+    cout << "__ Catring Service __" << endl;
+    cout << "Number of people: " << num_of_people   << endl;
+    cout << "Price: "           << calculatePrice() << endl;
+}
+
+Catring::~Catring() {
 }
