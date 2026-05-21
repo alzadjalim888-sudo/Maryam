1ed55e285a57712f245e68464f6cf16971828774
24-0153 <24-0153@student.gutech.edu.om>
2026-05-21 20:48:39 +0400

    Update catring.cpp

--- catring.cpp
+++ catring.cpp
@@ -16,6 +16,8 @@ double Catring::calculatePrice() {
     cout<<2-(100-150)<<endl;
     cout<<3-(150-200)<<endl;
     cin>>num_of_people;
+
+    
     switch (num_of_people) {
         
         case 1: return 100;
@@ -24,10 +26,19 @@ double Catring::calculatePrice() {
         default: return 0;
     }
 }
+int people(){
+switch (num_of_people) {
+        
+        case 1: cout<<"50-100 people";
+        case 2: cout<<"100-150 people";
+        case 3: cout<<"150-200 people";
+        default: return 0;
+    }
 
-void Catring::display(Event& event) {
+
+void Catring::display() {
     cout << "_____ Catring Service _____" << endl;
-    cout << "Number of people: " << event.getrange()  << endl;
+    cout << "Number of people: " << people()  << endl;
     cout << "Price: $"           << calculatePrice() << endl;
 }
 
