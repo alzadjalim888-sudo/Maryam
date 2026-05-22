c9feaabd39436634a2e01a5f1b86a4578c9daabc
Deenaalazzeh <deenaalazzeh805@gmail.com>
2026-05-23 03:32:17 +0400

    Update Venue_Class.cpp

--- Venue_Class.cpp
+++ Venue_Class.cpp
@@ -10,11 +10,12 @@ Venue::Venue(int vs) {
     venue_no = vs;}
 
 void Venue::VenueOptions() {
-    cout << "Venues:\n";
-    cout << "1. Garden\n" << setw(5) << "Price: 100 omr\n";
-    cout << "2. Home\n" << setw(5) << "Price: 50 omr\n";
-    cout << "3. Hall\n" << setw(5) << "Price: 200 omr\n";
+    cout << "\nVENUE OPTIONS:\n";
+    cout << "1. Garden (100 OMR)\n";
+    cout << "2. Home   (50 OMR)\n";
+    cout << "3. Hall   (200 OMR)\n";
 }
+
 
 int Venue::setVenue(int vs) {
     if (1 <= vs && vs <= 3) {
@@ -24,6 +25,13 @@ int Venue::setVenue(int vs) {
 
 int Venue::getVenue_no() {
     return venue_no;}
+
+int Venue::getPrice() {
+    if (venue_no == 1) return 100;
+    else if (venue_no == 2) return 50;
+    else if (venue_no == 3) return 200;
+    else return 0;
+}
 
 Venue::~Venue() {
 }
